275013116874779b010772f79010629e7ec42da7
Alex29f <aleksandrs0803@inbox.lv>
2025-02-07 11:23:06 +0200

    chapter 14

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     console.log('Props received:', props);
@@ -14,6 +15,9 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
             fetchInvoiceById(id),
             fetchCustomers(),
         ]);
+        if (!invoice){
+            notFound();
+        }
         console.log('Invoice:', invoice);
         console.log('Customers:', customers);
         
